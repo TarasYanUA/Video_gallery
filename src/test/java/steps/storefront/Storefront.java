d4698935e6bf2909cf675aaa29e09f07f4d6dbc7
Taras-Mila <w@gmail.com>
2024-07-11 20:33:37 +0300

    Improved method "checkAsserts".

--- src/test/java/steps/storefront/Storefront.java
+++ src/test/java/steps/storefront/Storefront.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.testng.asserts.SoftAssert;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class Storefront {
@@ -22,7 +23,7 @@ public class Storefront {
                 "There is no tab 'Video gallery' on the product page!");
         //Проверяем, что во вкладке ровно 4 видео
         $("#ab__video_gallery").click();
-        softAssert.assertTrue($$(".ab__vg-video").size() == 4,
+        softAssert.assertEquals($$(".ab__vg-video").size(), 4,
                 "There is no 4 videos on the tab 'Video gallery' on the product page!");
         softAssert.assertAll();
         takeScreenshot("@10 Четыре видео во вкладке товара");
