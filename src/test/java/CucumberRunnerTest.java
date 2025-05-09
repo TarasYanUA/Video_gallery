e25b40af17f8957a6f83faee4ed76b23f82ccccf
Taras-Mila <w@gmail.com>
2025-05-09 15:33:03 +0300

    Updated pom.xml vocabularies.

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -5,8 +5,13 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/features",
         glue = {"hooks", "steps"},
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json"},
-        tags = "@10_AddVideo or @20_Gallery01_VerticalView or @30_Gallery02_HorizontalView or @40_NoGallery01_HorizontalView or @50_NoGallery02_VerticalView or @60_Autoplay01_IconTypeNone_NoProductList or @65_Autoplay02_IconTypeNone_ProductList or @70_Autoplay03_IconTypeManualIcon or @80_Autoplay04_AllProductTemplates or @90_VideoAsDefaultProductImage"
-)
-
+        tags = "@10_AddVideo or @50_NoGallery02_VerticalView")
+/*
+@10_AddVideo or
+@20_Gallery01_VerticalView or @30_Gallery02_HorizontalView or
+@40_NoGallery01_HorizontalView or @50_NoGallery02_VerticalView or
+@60_Autoplay01_IconTypeNone_NoProductList or @65_Autoplay02_IconTypeNone_ProductList or @70_Autoplay03_IconTypeManualIcon or @80_Autoplay04_AllProductTemplates or
+@90_VideoAsDefaultProductImage
+*/
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
 }
