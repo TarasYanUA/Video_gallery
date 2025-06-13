6c5ad39b0051833c25a7226d00c5924511a69d7a
Taras-Mila <w@gmail.com>
2025-06-13 19:38:05 +0300

    Improved some code for video adding.

--- src/test/java/steps/adminPanel/AddDifferentVideosToProduct.java
+++ src/test/java/steps/adminPanel/AddDifferentVideosToProduct.java
@@ -14,46 +14,31 @@ public class AddDifferentVideosToProduct {
 
         if ($$("tr[id*='ab__vg_video_extra']").size() == 1) {
             //Добавляем видео с YouTube
-            $x("//input[@name='product_data[ab__vg_videos][1][pos]']").click();
             $x("//input[@name='product_data[ab__vg_videos][1][pos]']").setValue("10");
-            $(By.name("product_data[ab__vg_videos][1][title]")).click();
             $(By.name("product_data[ab__vg_videos][1][title]")).setValue("Музыка, успокаивает нервную систему и радует душу");
-            $("#ab__vg__video_path__1").click();
             $("#ab__vg__video_path__1").setValue("ayjzgSwtzbY");
 
             //Добавляем видео с Vimeo
             $("#box_add_ab__vg_video .btn-add").click();
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[pos]')])[2]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[pos]')])[2]").setValue("20");
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[title]')])[2]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[title]')])[2]").setValue("Disney Channel/ABC Oscar");
-            $x("(//select[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[type]')])[2]").click();
             $x("(//select[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[type]')])[2]").selectOption("Vimeo");
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[video_path]')])[2]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[video_path]')])[2]").setValue("154625007");
 
             //Добавляем видео типа "Ссылка"
-            $x("//tbody[contains(@id, 'box_add_ab__vg_video_')]//a[@class='btn btn-add']").click();
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[pos]')])[3]").click();
+            $x("(//tbody[contains(@id, 'box_add_ab__vg_video_')]//a[@class='btn btn-add'])[1]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[pos]')])[3]").setValue("30");
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[title]')])[3]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[title]')])[3]").setValue("Ego TV serial");
-            $x("(//select[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[type]')])[3]").click();
             $x("(//select[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[type]')])[3]").selectOption("Ссылка");
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[video_path]')])[3]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[video_path]')])[3]")
                     .setValue("https://ashdi.vip/vod/91121");
 
             //Добавляем видео типа "Ресурс"
             $x("(//tbody[contains(@id, 'box_add_ab__vg_video_')]//a[@class='btn btn-add'])[2]").click();
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[pos]')])[4]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[pos]')])[4]").setValue("40");
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[title]')])[4]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[title]')])[4]")
                     .setValue("Fire Long name: Значение этой настройки будет напрямую встроено в код. Будьте внимательны, заполняя её значение");
-            $x("(//select[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[type]')])[4]").click();
             $x("(//select[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[type]')])[4]").selectOption("Ресурс");
-            $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[video_path]')])[4]").click();
             $x("(//input[contains(@name, 'product_data[ab__vg_videos]')][contains(@name, '[video_path]')])[4]")
                     .setValue("https://unitheme.net/images/ut2_banner_videos/33_52_ABSTRACT.mp4");
         }
