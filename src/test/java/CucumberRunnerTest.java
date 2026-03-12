ddda810772de09189ab7cac1bdfefe13e02e499e
TarasYan <wildmonach@gmail.com>
2026-03-12 18:00:24 +0200

    Fixed a bug in assert for video with autoplay.

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -5,12 +5,12 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/features",
         glue = {"hooks", "steps"},
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json"},
-        tags = "@10_AddVideo")
+        tags = "@90_VideoAsDefaultProductImage")
 /*
 @10_AddVideo or
 @20_Gallery01_VerticalView or @30_Gallery02_HorizontalView or
 @40_NoGallery01_HorizontalView or @50_NoGallery02_VerticalView or
-@60_Autoplay01_IconTypeNone_NoProductList or @65_Autoplay02_IconTypeNone_ProductList or @70_Autoplay03_IconTypeManualIcon or @80_Autoplay04_AllProductTemplates or
+@60_Autoplay01_IconTypeNone_NoProductList or @65_Autoplay02_IconTypeNone_ProductList or @70_Autoplay03_IconTypeManualIcon or @80_Autoplay04_AllProductTemplates
 @90_VideoAsDefaultProductImage
 */
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
